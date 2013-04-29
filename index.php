5164626f620038f949b8381ae81dad815f73800b
Mario Kraemer <kraem01@cougars.csusm.edu>
2013-04-29 14:12:41 -0700

    Change in display
    If there is no book in a chosen category, there will be a message.

--- index.php
+++ index.php
@@ -25,11 +25,16 @@ function content($connection) {
 		<?php
 			$result_offer= $connection->query($sql_offer);
 			if(isset($_GET["category"])) {
-				while ($row_offer = $result_offer->fetch_object()) {		
-		?>  
-				<tr><td><a href="bookDetails.php?offerid=<?=$row_offer->OfferId?>"><?=$row_offer->Title?></a></td>
-				<td><?=$row_offer->Author?></td><td><?=$row_offer->Price?></td></tr>
-		<?php
+				$num_rows = mysqli_num_rows($result_offer);
+				if ($num_rows == 0) {
+					echo "<tr><td colspan=\"4\">Sorry, there are no books in the chosen category.</td></tr>";
+				} else {
+					while ($row_offer = $result_offer->fetch_object()) {		
+			?>  
+					<tr><td><a href="bookDetails.php?offerid=<?=$row_offer->OfferId?>"><?=$row_offer->Title?></a></td>
+					<td><?=$row_offer->Author?></td><td><?=$row_offer->Price?></td></tr>
+			<?php
+					}
 				}
 			} else {
 				while ($row_offer = $result_offer->fetch_object()) {
