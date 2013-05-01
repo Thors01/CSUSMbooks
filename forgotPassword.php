81125e9bcbce34777188d83959a933252cf4184e
Mario Kraemer <kraem01@cougars.csusm.edu>
2013-05-01 16:21:57 -0700

    Validation of csusm.edu and cougars.csusm.edu

--- forgotPassword.php
+++ forgotPassword.php
@@ -67,7 +67,7 @@ function content($connection) {
 			<tbody>
 				<tr>
 					<td><label for="mail">Your mail:</label></td>
-					<td><input type="text" name="mail" id="mail" data-validation-pattern="^[_a-z0-9-]+(.[_a-z0-9-]+)*@csusm.edu$" data-validation-message="Please enter a valid @csusm.edu mail address." /></td>
+					<td><input type="text" name="mail" id="mail" data-validation-pattern="^[_a-z0-9-]+(.[_a-z0-9-]+)*@(.[_a-z0-9-]*)*csusm.edu$" data-validation-message="Please enter a valid @csusm.edu mail address." /></td>
 				</tr>
 				<tr>
 					<td><label for="confirmmail">Confirm mail:</label></td>
--- login.php
+++ login.php
@@ -41,7 +41,7 @@ function content($connection) {
 			<tbody>
 				<tr>
 					<td><label for="mail">Mail:</label></td>
-					<td><input type="text" name="mail" id="mail" data-validation-pattern="^[_a-z0-9-]+(.[_a-z0-9-]+)*@csusm.edu$" data-validation-message="Please enter a valid @csusm.edu mail address." /></td>
+					<td><input type="text" name="mail" id="mail" data-validation-pattern="^[_a-z0-9-]+(.[_a-z0-9-]+)*@(.[_a-z0-9-]*)*csusm.edu$" data-validation-message="Please enter a valid @csusm.edu mail address." /></td>
 				</tr>
 				<tr>
 					<td><label for="password">Password:</label></td>
